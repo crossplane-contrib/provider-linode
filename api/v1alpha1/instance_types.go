0652b7051973044728ce72eecc88df1ed6e3607c
Marques Johansson <marques@displague.com>
2019-09-20 17:15:01 -0400

    implement crossplane-runtime managed resource requirements

--- api/v1alpha1/instance_types.go
+++ api/v1alpha1/instance_types.go
@@ -17,7 +17,10 @@ limitations under the License.
 package v1alpha1
 
 import (
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	runtimev1alpha1 "github.com/crossplaneio/crossplane-runtime/apis/core/v1alpha1"
 )
 
 // EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
@@ -27,12 +30,14 @@ import (
 type InstanceSpec struct {
 	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
+	runtimev1alpha1.ResourceSpec `json:",inline"`
 }
 
 // InstanceStatus defines the observed state of Instance
 type InstanceStatus struct {
 	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
+	runtimev1alpha1.ResourceStatus `json:",inline"`
 }
 
 // +kubebuilder:object:root=true
@@ -44,6 +49,76 @@ type Instance struct {
 
 	Spec   InstanceSpec   `json:"spec,omitempty"`
 	Status InstanceStatus `json:"status,omitempty"`
+}
+
+// GetSpec returns the MySQL server's spec.
+func (s *Instance) GetSpec() *InstanceSpec {
+	return &s.Spec
+}
+
+// GetStatus returns the MySQL server's status.
+func (s *Instance) GetStatus() *InstanceStatus {
+	return &s.Status
+}
+
+// SetStatus sets the MySQL server's status.
+func (s *Instance) SetStatus(status *InstanceStatus) {
+	s.Status = *status
+}
+
+// SetBindingPhase of this Instance.
+func (a *Instance) SetBindingPhase(p runtimev1alpha1.BindingPhase) {
+	a.Status.SetBindingPhase(p)
+}
+
+// GetBindingPhase of this Instance.
+func (a *Instance) GetBindingPhase() runtimev1alpha1.BindingPhase {
+	return a.Status.GetBindingPhase()
+}
+
+// SetConditions of this Instance.
+func (a *Instance) SetConditions(c ...runtimev1alpha1.Condition) {
+	a.Status.SetConditions(c...)
+}
+
+// SetClaimReference of this Instance.
+func (a *Instance) SetClaimReference(r *corev1.ObjectReference) {
+	a.Spec.ClaimReference = r
+}
+
+// GetClaimReference of this Instance.
+func (a *Instance) GetClaimReference() *corev1.ObjectReference {
+	return a.Spec.ClaimReference
+}
+
+// SetNonPortableClassReference of this Instance.
+func (a *Instance) SetNonPortableClassReference(r *corev1.ObjectReference) {
+	a.Spec.NonPortableClassReference = r
+}
+
+// GetNonPortableClassReference of this Instance.
+func (a *Instance) GetNonPortableClassReference() *corev1.ObjectReference {
+	return a.Spec.NonPortableClassReference
+}
+
+// SetWriteConnectionSecretToReference of this Instance.
+func (a *Instance) SetWriteConnectionSecretToReference(r corev1.LocalObjectReference) {
+	a.Spec.WriteConnectionSecretToReference = r
+}
+
+// GetWriteConnectionSecretToReference of this Instance.
+func (a *Instance) GetWriteConnectionSecretToReference() corev1.LocalObjectReference {
+	return a.Spec.WriteConnectionSecretToReference
+}
+
+// GetReclaimPolicy of this Instance.
+func (a *Instance) GetReclaimPolicy() runtimev1alpha1.ReclaimPolicy {
+	return a.Spec.ReclaimPolicy
+}
+
+// SetReclaimPolicy of this Instance.
+func (a *Instance) SetReclaimPolicy(p runtimev1alpha1.ReclaimPolicy) {
+	a.Spec.ReclaimPolicy = p
 }
 
 // +kubebuilder:object:root=true
